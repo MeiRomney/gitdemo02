f22e2e4b1a78be9bf3a5f8b7b3cfec092376c52f
Mei Romney <mei.romney987@gmail.com>
2025-09-16 11:56:39 +0200

    Add a and b methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(a() + b());
     }
+
+    public static String b() {
+        return "world! ";
+    }
+
+    public static String a() {return "hello! "; }
 }
